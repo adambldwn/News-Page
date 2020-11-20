ba8c4aeca70da9eaca3854a0cc6adce8f335e3de
efe <adambldwn110@gmail.com>
2020-11-20 16:44:52 +0300

    NewsPage banner

--- v_2/components/NewsCard.js
+++ v_2/components/NewsCard.js
@@ -1,37 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
-
+import { View, Image, Text, StyleSheet, Dimensions} from 'react-native';
 
 const NewsCard = ({news}) => {
-    return (
+    return(
         <View style={styles.container}>
-            <Image
-                source={{uri: news.imageUrl}}
-                style={styles.image}
-            />
+            <Image source={{uri: news.imageUrl}} style={styles.image}/>
             <Text style={styles.title}>{news.title}</Text>
-            <Text>{news.description}</Text>
+            <Text style={styles.description}>{news.description}</Text>
         </View>
     )
 }
 
-export { NewsCard };
-
+export {NewsCard};
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        padding:5,
-        margin:2,
+        flex:1,
         borderWidth: 1,
-        borderRadius: 5,
+        borderColor:'#eceff1',
+        padding:7,
+        margin:5,
+        borderRadius:5
     },
-    title: {
-        fontWeight: 'bold'
+    title:{
+        fontWeight:'bold',
+        fontSize:17,
     },
-    image: {
-        height: Dimensions.get('window').height * 0.3,
-        borderRadius: 5,
-        resizeMode: 'contain'
+    image:{
+        height: Dimensions.get('window').height * 0.25,
+        borderRadius:5,
+        resizeMode:'contain'
     }
 })
